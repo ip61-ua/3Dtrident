806c1ab7853df84e3c6a5e8d43fcda05267d11d1
ip61-ua <ip61@alu.ua.es>
2025-02-22 22:44:38 +0100

    Aplicar uso de externalizaciones en Screen

--- include/screen.h
+++ include/screen.h
@@ -4,11 +4,12 @@
 #include <3ds.h>
 #include <citro2d.h>
 
-struct console {
-  C3D_RenderTarget *top = C2D_CreateScreenTarget (GFX_TOP, GFX_LEFT);
-  C3D_RenderTarget *bottom = C2D_CreateScreenTarget (GFX_BOTTOM, GFX_LEFT);
-  C2D_TextBuf g_staticBuf = C2D_TextBufNew (4096);
-  C2D_Font font = C2D_FontLoad ("romfs:/cascadia.bcfnt");
+namespace Screen
+{
+extern C3D_RenderTarget *top;
+extern C3D_RenderTarget *bottom;
+extern C2D_TextBuf g_staticBuf;
+extern C2D_Font font;
 }
 
 #endif
--- source/screen.cpp
+++ source/screen.cpp
@@ -1,1 +1,9 @@
 #include "screen.h"
+
+namespace Screen
+{
+C3D_RenderTarget *top = C2D_CreateScreenTarget (GFX_TOP, GFX_LEFT);
+C3D_RenderTarget *bottom = C2D_CreateScreenTarget (GFX_BOTTOM, GFX_LEFT);
+C2D_TextBuf g_staticBuf = C2D_TextBufNew (4096);
+C2D_Font font = C2D_FontLoad ("romfs:/cascadia.bcfnt");
+}
