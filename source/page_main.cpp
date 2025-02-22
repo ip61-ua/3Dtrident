d4e7b984b957b3285ecdd1be8469cbdb5c87e89a
ip61-ua <ip61@alu.ua.es>
2025-02-22 22:50:35 +0100

    Cambiar fondo de inicio de pantalla superior.

--- source/page_main.cpp
+++ source/page_main.cpp
@@ -5,7 +5,7 @@ namespace PAGE_MAIN
 void showPage () {
   C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 
-  C2D_TargetClear(Screen::top, C2D_Color32(0x68, 0xB0, 0xD8, 0xFF));
+  C2D_TargetClear(Screen::top, C2D_Color32(29, 34, 39, 255));
   C2D_SceneBegin(Screen::top);
   C3D_FrameEnd(0);
 }
