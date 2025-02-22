ee2fe0df05d3a04a0d68c450dd541470d33c0c0d
ip61-ua <ip61@alu.ua.es>
2025-02-22 19:50:20 +0100

    Borrar experimentaciones.

--- source/main.cpp
+++ source/main.cpp
@@ -1,65 +1,18 @@
-#include "screen.h"
 #include <3ds.h>
-#include <citro2d.h>
-
-C2D_TextBuf g_staticBuf;
-C2D_Text g_staticText[2];
-C2D_Font font[2];
-
-static void
-sceneInit (void)
-{
-  g_staticBuf = C2D_TextBufNew (4096);
-  font[0] = C2D_FontLoadSystem (CFG_REGION_USA);
-  font[1] = C2D_FontLoadSystem (CFG_REGION_KOR);
-
-  // Parse the text strings
-  // Loads system font
-  C2D_TextFontParse (&g_staticText[0], font[0], g_staticBuf,
-                     "A boring system font.");
-  // Uses loaded font
-  C2D_TextFontParse (&g_staticText[1], font[1], g_staticBuf,
-                     "이 텍스트는 한국어입니다.");
-  // Uses other loaded font
-
-  // Optimize the text strings
-  C2D_TextOptimize (&g_staticText[0]);
-  C2D_TextOptimize (&g_staticText[1]);
-}
-
-static void
-sceneExit (void)
-{
-  // Delete the text buffers
-  C2D_TextBufDelete (g_staticBuf);
-  C2D_FontFree (font[0]);
-  C2D_FontFree (font[1]);
-}
+#include "screen.h"
+#include "main_menu.h"
 
 int
 main ()
 {
-  setupControls ();
+  Screen::initProgram();
+  setupControls();
 
-  // Main loop
   while (aptMainLoop ())
     {
-      hidScanInput ();
-
-      // Render the scene
-      C3D_FrameBegin (C3D_FRAME_SYNCDRAW);
-      showControls ();
-      C3D_FrameEnd (0);
+      showControls();
     }
 
-  // Deinitialize the scene
-  sceneExit ();
-
-  // Deinitialize the libs
-  C2D_Fini ();
-  C3D_Fini ();
-  romfsExit ();
-  cfguExit ();
-  gfxExit ();
+  Screen::endProgram();
   return 0;
 }
