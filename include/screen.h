2bad2a0fc161209f5ca7071dd0e6931f54767cdd
ip61-ua <ip61@alu.ua.es>
2025-02-22 22:33:10 +0100

    Crear struct console en screen.h

--- include/screen.h
+++ include/screen.h
@@ -4,12 +4,11 @@
 #include <3ds.h>
 #include <citro2d.h>
 
-namespace Screen {
-  static C3D_RenderTarget *top = C2D_CreateScreenTarget (GFX_TOP, GFX_LEFT);
-  static C3D_RenderTarget *bottom = C2D_CreateScreenTarget (GFX_BOTTOM, GFX_LEFT);
-
-  static C2D_TextBuf g_staticBuf = C2D_TextBufNew (4096);
-  static C2D_Font font = C2D_FontLoad ("romfs:/cascadia.bcfnt");
-};
+struct console {
+  C3D_RenderTarget *top = C2D_CreateScreenTarget (GFX_TOP, GFX_LEFT);
+  C3D_RenderTarget *bottom = C2D_CreateScreenTarget (GFX_BOTTOM, GFX_LEFT);
+  C2D_TextBuf g_staticBuf = C2D_TextBufNew (4096);
+  C2D_Font font = C2D_FontLoad ("romfs:/cascadia.bcfnt");
+}
 
 #endif
