358006ee12a7bf681f7f3948794bf6b8d2bec984
ip61-ua <ip61@alu.ua.es>
2025-02-23 08:18:03 +0100

    Añadir representación para cstick

--- source/page_main.cpp
+++ source/page_main.cpp
@@ -8,13 +8,15 @@ showPage ()
   Hardware_listenInput ();
   Screen::setBackground (Screen::top, C2D_Color32 (29, 34, 39, 255));
 
-  circlePosition pos;
-  Hardware_CirclePad(&pos);
+  circlePosition circle_pos, cstick_pos;
+  Hardware_CirclePad(&circle_pos);
+  Hardware_CStick(&cstick_pos);
 
   u32 gray = C2D_Color32 (85, 88, 91, 255);
-  C2D_DrawCircle (100.0f + pos.dx/5.0f, 100.0f - pos.dy/5.0f, 0, 20, gray, gray, gray, gray);
+  C2D_DrawCircle (60.0f + circle_pos.dx/5.0f, 80.0f - circle_pos.dy/5.0f, 0, 20, gray, gray, gray, gray);
+  C2D_DrawCircle (300.0f + cstick_pos.dx/5.0f, 70.0f - cstick_pos.dy/5.0f, 0, 10, gray, gray, gray, gray);
 
-  Screen::setBackground (Screen::bottom, C2D_Color32 (29, 34, 39, 255));
+  //Screen::setBackground (Screen::bottom, C2D_Color32 (29, 34, 39, 255));
   if (Hardware_A ()) Application::PAGE_CURRENT = Application::PAGE_ABOUT;
 }
 };
