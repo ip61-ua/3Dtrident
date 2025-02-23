6f554cd4e29663024c7d26d8a12455618f8cc731
ip61-ua <ip61@alu.ua.es>
2025-02-23 07:56:53 +0100

    Ajustar sensibilidad joystick.

--- source/page_main.cpp
+++ source/page_main.cpp
@@ -12,10 +12,9 @@ showPage ()
   Hardware_CirclePad(&pos);
 
   u32 gray = C2D_Color32 (85, 88, 91, 255);
-  C2D_DrawCircle (100.0f + pos.dx/10.0f, 100.0f - pos.dy/10.0f, 0, 20, gray, gray, gray, gray);
+  C2D_DrawCircle (100.0f + pos.dx/5.0f, 100.0f - pos.dy/5.0f, 0, 20, gray, gray, gray, gray);
 
   Screen::setBackground (Screen::bottom, C2D_Color32 (29, 34, 39, 255));
-  if (Hardware_A ())
-    Application::PAGE_CURRENT = Application::PAGE_ABOUT;
+  if (Hardware_A ()) Application::PAGE_CURRENT = Application::PAGE_ABOUT;
 }
 };
