481f5c07fec7f05fcd7b0e3041785bb3d3adbef2
ip61-ua <ip61@alu.ua.es>
2025-02-24 09:17:11 +0100

    Refactor de displayABXY

--- source/page_main.c
+++ source/page_main.c
@@ -1,41 +1,37 @@
 #include "page_main.h"
 
 void
+displayABXY_updateVars (unsigned *y_dst, u32 *c_dst, const int new_rel_y,
+                        const u32 new_c)
+{
+  *y_dst += new_rel_y;
+  *c_dst = new_c;
+}
+
+void
 displayABXY (float x, float y)
 {
   u32 color_a, color_b, color_y, color_x,
       btn_default = C2D_Color32 (253, 253, 253, 255);
 
-  unsigned r_a, r_b, r_y, r_x, r_default = 11, r_pressed = 1;
+  unsigned y_a = 125, y_b = 150, y_x = 100, y_y = 125;
 
-  r_a = r_b = r_y = r_x = r_default;
   color_a = color_b = color_y = color_x = btn_default;
 
   if (Hardware_A ())
-    {
-      color_a = C2D_Color32 (247, 56, 0, 255);
-      r_a -= r_pressed;
-    }
+    displayABXY_updateVars (&y_a, &color_a, +2, C2D_Color32 (247, 56, 0, 255));
   if (Hardware_B ())
-    {
-      color_b = C2D_Color32 (248, 183, 2, 255);
-      r_b -= r_pressed;
-    }
+    displayABXY_updateVars (&y_b, &color_b, +2,
+                            C2D_Color32 (248, 183, 2, 255));
   if (Hardware_Y ())
-    {
-      color_y = C2D_Color32 (0, 184, 0, 255);
-      r_y -= r_pressed;
-    }
+    displayABXY_updateVars (&y_y, &color_y, +2, C2D_Color32 (0, 184, 0, 255));
   if (Hardware_X ())
-    {
-      color_x = C2D_Color32 (2, 87, 247, 255);
-      r_x -= r_pressed;
-    }
+    displayABXY_updateVars (&y_x, &color_x, +2, C2D_Color32 (2, 87, 247, 255));
 
-  Screen_drawCircle (330, 100, r_x, color_x);
-  Screen_drawCircle (360, 125, r_a, color_a);
-  Screen_drawCircle (330, 150, r_b, color_b);
-  Screen_drawCircle (300, 125, r_y, color_y);
+  Screen_drawCircle (330, y_x, 11, color_x);
+  Screen_drawCircle (360, y_a, 11, color_a);
+  Screen_drawCircle (330, y_b, 11, color_b);
+  Screen_drawCircle (300, y_y, 11, color_y);
 }
 
 void
