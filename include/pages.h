e6c8747307227d198f0e211fcf4a9f168b85117e
ip61-ua <ip61@alu.ua.es>
2025-03-11 19:03:07 +0100

    Documentar pages.h

--- include/pages.h
+++ include/pages.h
@@ -6,14 +6,27 @@
 
 typedef void EntryPage;
 typedef EntryPage (*Page) ();
-extern Page PAGE_CURRENT;
 
-// interface
+extern Page PAGE_CURRENT;
+// Interfaz para la declaración de las páginas que hay disponibles.
 extern Page PAGE_MAIN;
 extern Page PAGE_ABOUT;
-// end
+// FIN.
 
-void Page_setup (bool *, void (*) ());
-void Page_changeTo (Page, void (*) ());
+/**
+  @brief Mecanismo de actuación para la inicialización de una página.
+  @param cond Puntero de booleano que se actualiza su valor, al ejecutarse
+  la función start. No se actualiza si no se ejecuta.
+  @param start Función "callback" a ejecutar para inicializar la visualización
+  de página.
+ */
+void Page_setup (bool *cond, void (*start) ());
+
+/**
+ * @brief Cambia a la página solicitada, previamente llamando a quit.
+ * @param page Página a la que cambiar.
+ * @param quit Función "callback" a la que llamar antes del cambio.
+ */
+void Page_changeTo (Page page, void (*quit) ());
 
 #endif
