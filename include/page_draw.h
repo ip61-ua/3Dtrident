8f96d8aa29a836e5f361a1e6d7aa5823d5c25046
ip61-ua <ip61@alu.ua.es>
2025-03-17 22:02:24 +0100

    Espaciar texto de ayuda.

--- include/page_draw.h
+++ include/page_draw.h
@@ -9,15 +9,15 @@
 #include <3ds.h>
 #include <citro2d.h>
 
-#define PAGE_HELP_TEXT_MINUS EMOJI_D_PAD_U "Aumentar radio/grosor de dibujado."
-#define PAGE_HELP_TEXT_PLUS EMOJI_D_PAD_D "Reducir radio/grosor de dibujado."
+#define PAGE_HELP_TEXT_MINUS EMOJI_D_PAD_U " Aumentar radio/grosor de dibujado."
+#define PAGE_HELP_TEXT_PLUS EMOJI_D_PAD_D " Reducir radio/grosor de dibujado."
 #define PAGE_HELP_TEXT_BGCO                                                   \
-  EMOJI_D_PAD_LR "Alternar tema de dibujo."
+  EMOJI_D_PAD_LR " Alternar tema de dibujo."
 #define PAGE_HELP_TEXT_TRACE                                                  \
-  EMOJI_A_BUTTON "Mostrar/Ocultar trazado entre toques."
-#define PAGE_HELP_TEXT_HIDE EMOJI_Y_BUTTON "Mostrar/Ocultar este texto."
-#define PAGE_HELP_TEXT_CLEAN EMOJI_X_BUTTON "Limpiar pantalla táctil."
-#define PAGE_HELP_TEXT_BACK EMOJI_B_BUTTON "Volver al menú."
+  EMOJI_A_BUTTON " Mostrar/Ocultar traza entre toques."
+#define PAGE_HELP_TEXT_HIDE EMOJI_Y_BUTTON " Mostrar/Ocultar este texto."
+#define PAGE_HELP_TEXT_CLEAN EMOJI_X_BUTTON " Limpiar pantalla táctil."
+#define PAGE_HELP_TEXT_BACK EMOJI_B_BUTTON " Volver al menú."
 
 #define PAGE_HELP_TEXT                                                        \
   PAGE_HELP_TEXT_MINUS "\n" PAGE_HELP_TEXT_PLUS "\n" PAGE_HELP_TEXT_BGCO      \
